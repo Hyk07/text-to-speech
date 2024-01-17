a2dd253b67c5497f9b5893964214ea81ba2eeb4e
Hyk07 <theorder.cp@gmail.com>
2024-01-17 16:22:47 +0300

    v1

---
+++ speechProject/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace speechProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBarVol = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOku = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVol)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 47);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 334);
+            this.textBox1.TabIndex = 0;
+            // 
+            // trackBarVol
+            // 
+            this.trackBarVol.Location = new System.Drawing.Point(684, 387);
+            this.trackBarVol.Maximum = 100;
+            this.trackBarVol.Name = "trackBarVol";
+            this.trackBarVol.Size = new System.Drawing.Size(104, 56);
+            this.trackBarVol.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(625, 405);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Volume";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(241, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 38);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TEXT TO SPEECH";
+            // 
+            // btnOku
+            // 
+            this.btnOku.Location = new System.Drawing.Point(12, 394);
+            this.btnOku.Name = "btnOku";
+            this.btnOku.Size = new System.Drawing.Size(88, 29);
+            this.btnOku.TabIndex = 5;
+            this.btnOku.Text = "Oku";
+            this.btnOku.UseVisualStyleBackColor = true;
+            this.btnOku.Click += new System.EventHandler(this.btnOku_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(136, 397);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnOku);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBarVol);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVol)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBarVol;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOku;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ speechProject/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech;
+using System.Speech.AudioFormat;
+
+
+namespace speechProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOku_Click(object sender, EventArgs e)
+        {
+            reader.SpeakAsync(textBox1.Text);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var voice = comboBox1.SelectedItem as String;
+            if (voice != null)
+
+            {
+                reader.SelectVoice(voice);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        
+        {
+            PopulateInstalledVoices();
+        }
+        
+        private SpeechSynthesizer reader = new SpeechSynthesizer();
+        private void PopulateInstalledVoices()
+
+        {
+            foreach (InstalledVoice voice in reader.GetInstalledVoices())
+
+            {
+                VoiceInfo info = voice.VoiceInfo;
+
+                comboBox1.Items.Add(info.Name);
+            }
+        }
+    }
+    }
+    
+
